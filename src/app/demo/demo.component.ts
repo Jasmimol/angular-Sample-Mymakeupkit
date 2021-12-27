6be2d098253db87c0b2a6e813bd89b19574a301e
Jasmimol <bjasmimol@gmail.com>
2021-12-27 17:31:10 +0400

    3 files modified, 1 added

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,24 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Product} from '../product';
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css'],
 })
 export class DemoComponent implements OnInit {
-  products = [
+  products : Product[]  = [
     { prod: 'Lipstick', brand: 'Chanel' },
     { prod: 'Eyeliner', brand: 'Sephora' },
     { prod: 'Mascara', brand: 'Loreal' },
     { prod: 'Rouge', brand: 'Gucci' },
-    { prod: 'Nail Polish', brand: 'Loreal' },
-  ];
+    { prod: 'Nail Polish', brand: 'Loreal' }]
+  ;
+  selectedProduct?: Product;
 
-  Displayoffers() {
 
-  }
-  Displayproducts() {}
   constructor() {}
 
   ngOnInit() {}
+  onSelect(product: Product): void {
+    this.selectedProduct = product;
+  }
 }
