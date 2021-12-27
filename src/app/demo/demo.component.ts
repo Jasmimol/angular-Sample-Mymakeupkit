876cb33aed3b9b5e860e4e840354f6fcbf1a4e50
Jasmimol <bjasmimol@gmail.com>
2021-12-27 09:42:08 +0400

    3 files modified

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
-  styleUrls: ['./demo.component.css']
+  styleUrls: ['./demo.component.css'],
 })
 export class DemoComponent implements OnInit {
-  products =[{'prod':'Lipstick','brand':'Chanel'},{'prod':'Eyeliner','brand':'Sephora'},{'prod':'Mascara','brand':'Loreal'},{'prod':'Rogue','brand':'Gucci'},{'prod':'Nail Polish','brand':'Loreal'}];
-  constructor() { }
+  products = [
+    { prod: 'Lipstick', brand: 'Chanel' },
+    { prod: 'Eyeliner', brand: 'Sephora' },
+    { prod: 'Mascara', brand: 'Loreal' },
+    { prod: 'Rouge', brand: 'Gucci' },
+    { prod: 'Nail Polish', brand: 'Loreal' },
+  ];
 
-  ngOnInit() {
+  Displayoffers() {
+
   }
+  Displayproducts() {}
+  constructor() {}
 
+  ngOnInit() {}
 }
